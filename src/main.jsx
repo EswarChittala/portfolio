18eceb619d35510b48286deb832b07e8fabb7d97
EswarChittala <eswarchittala13@gmail.com>
2025-09-23 20:32:51 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import "./styles/globals.css";  // Make sure this path is correct
+import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
